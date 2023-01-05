61dec2e8dd18b61f749c430bfd1c5c79db709526
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-01-04 20:43:23 -0500

    added custom ico and tailwind font

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-3xl font-bold underline">Hello Portfolio!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
