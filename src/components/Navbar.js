1635a2fbd698fa4732ecda7d9df595359d262209
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-01-18 17:31:59 -0500

    modified navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,11 @@ function Navbar({ color, children }) {
             CYNTHIA BRITO-MENA
           </h1>
         </Link>
+
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -35,12 +36,15 @@ function Navbar({ color, children }) {
           </svg>
         </button>
 
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div
+          class="hidden w-full md:block md:w-auto text-navbarText"
+          id="navbar-default"
+        >
+          <ul class="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-light md:border-0">
             <li>
               <Link
                 to="/about"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                className="block py-2 pl-3 pr-4 text-navbarText"
                 aria-current="page"
               >
                 About Me
@@ -49,7 +53,7 @@ function Navbar({ color, children }) {
             <li>
               <Link
                 to="/projects"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-navbarText"
               >
                 Projects
               </Link>
@@ -57,7 +61,7 @@ function Navbar({ color, children }) {
             <li>
               <Link
                 to="/resume"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-navbarText"
               >
                 Resume
               </Link>
@@ -65,7 +69,7 @@ function Navbar({ color, children }) {
             <li>
               <Link
                 to="/contact"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-navbarText"
               >
                 Let's Connect
               </Link>
@@ -74,7 +78,7 @@ function Navbar({ color, children }) {
               <a
                 href="https://github.com/cb2866"
                 rel="noreferrer"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-navbarText"
               >
                 <GitHubIcon />
               </a>
@@ -83,7 +87,7 @@ function Navbar({ color, children }) {
               <a
                 href="https://linkedin.com/in/cynthiabritomena"
                 rel="noreferrer"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-navbarText"
               >
                 <LinkedInIcon />
               </a>
